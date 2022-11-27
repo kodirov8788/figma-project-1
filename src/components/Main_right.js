66ff3a66d29941a5e1791eff49e34b3956f27563
kodirov8788 <kodirov8788@gmail.com>
2022-11-28 01:30:46 +0500

    asdsdwww

--- src/components/Main_right.js
+++ src/components/Main_right.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Mainbg from "../images/mainbg.svg"
 function Main_right() {
     const stl = {
-        mainright: "w-[52%]  w-fit absolute z-1 right-0 top-[7em]"
+        mainright: "w-[51%]  w-fit absolute z-1 right-0 top-[7em]"
     }
     return (
         <div className={stl.mainright}>
-            <img src={Mainbg} alt="" className='translate-[50px]' />
+            <img src={Mainbg} alt="" />
         </div>
     )
 }
